0da2485eb852f8382c2f9d006ab11269a873c390
WhiteStatix <canakduruk@gmail.com>
2022-12-25 16:39:05 +0300

    Update

--- Assets/Scripts/BirdsScripts/BirdMovement.cs
+++ Assets/Scripts/BirdsScripts/BirdMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BirdMovement : MonoBehaviour
 {
     Vector2 enemyPos;
-    public float speed;
+    float speed;
 
     private void Start()
     {
@@ -13,6 +13,27 @@ public class BirdMovement : MonoBehaviour
     }
     private void Update()
     {
+        if (GiftToHouse.score >= 0 && GiftToHouse.score <= 5)
+        {
+            speed = 8;
+        }
+        else if (GiftToHouse.score >= 6 && GiftToHouse.score <= 10)
+        {
+            speed = 9f;
+        }
+        else if (GiftToHouse.score >= 11 && GiftToHouse.score <= 15)
+        {
+            speed = 10f;
+        }
+        else if (GiftToHouse.score >= 16 && GiftToHouse.score <= 20)
+        {
+            speed = 11f;
+        }
+        else if (GiftToHouse.score >= 21 && GiftToHouse.score <= 25)
+        {
+            speed = 13;
+        }
+        
         enemyPos = new Vector2(enemyPos.x + (-speed * Time.deltaTime), enemyPos.y);
         transform.position = enemyPos;
     }
--- Assets/Scripts/HousesScripts/HousesSpawner.cs
+++ Assets/Scripts/HousesScripts/HousesSpawner.cs
@@ -6,19 +6,37 @@ public class HousesSpawner : MonoBehaviour
 {
     [SerializeField] List<GameObject> houses = new List<GameObject> ();
     GameObject sentHouse;
+    float spawnerTime = 4;
     int random;
     
     [SerializeField] Transform spawnerPos;
 
     private void Start() 
     {
-        InvokeRepeating("HouseSpawn",1,2);
+        InvokeRepeating("HouseSpawn",1,spawnerTime);
     }
     private void Update() 
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        
+        if (GiftToHouse.score >= 0 && GiftToHouse.score <= 5)
         {
-            HouseSpawn();
+            spawnerTime = 4;
+        }
+        else if (GiftToHouse.score >= 6 && GiftToHouse.score <= 10)
+        {
+            spawnerTime = 3.5f;
+        }
+        else if (GiftToHouse.score >= 11 && GiftToHouse.score <= 15)
+        {
+            spawnerTime = 3;
+        }
+        else if (GiftToHouse.score >= 16 && GiftToHouse.score <= 20)
+        {
+            spawnerTime = 2.5f;
+        }
+        else if (GiftToHouse.score >= 21 && GiftToHouse.score <= 25)
+        {
+            spawnerTime = 2;
         }
         
     }
@@ -37,7 +55,8 @@ public class HousesSpawner : MonoBehaviour
     public void GetHouse()
     {
         sentHouse.SetActive(true);
-        sentHouse.GetComponent<Rigidbody2D>().velocity = Vector2.left * 4;
+        sentHouse.GetComponent<Rigidbody2D>().velocity = Vector2.left * 5;
+        
         houses.Add(sentHouse);
         sentHouse.transform.position = spawnerPos.position;
     }
